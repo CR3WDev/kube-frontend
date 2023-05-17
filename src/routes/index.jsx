6a5d6e600c04a04a034869e341e6297241c4d62b
Marcelo Távora <marcelovtneves@gmail.com>
2023-05-17 17:35:09 -0300

    🔥 Removido mais código lixo

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -6,10 +6,6 @@ import Logout from "../pages/Authentication/Logout";
 import Register from "../pages/Authentication/Register";
 import UserProfile from "../pages/Authentication/user-profile";
 import Chat from "../pages/Chat/Chat";
-import Pages404 from "../pages/Utility/pages-404";
-import Pages500 from "../pages/Utility/pages-500";
-import PagesComingsoon from "../pages/Utility/pages-comingsoon";
-import PagesMaintenance from "../pages/Utility/pages-maintenance";
 
 const authProtectedRoutes = [
   { path: "/dashboard", component: () => <Redirect to="/chat" /> },
@@ -23,11 +19,6 @@ const publicRoutes = [
   { path: "/login", component: Login },
   { path: "/forgot-password", component: ForgetPwd },
   { path: "/register", component: Register },
-
-  { path: "/pages-maintenance", component: PagesMaintenance },
-  { path: "/pages-comingsoon", component: PagesComingsoon },
-  { path: "/pages-404", component: Pages404 },
-  { path: "/pages-500", component: Pages500 },
 ];
 
 export { authProtectedRoutes, publicRoutes };
